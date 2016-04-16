c87e6ffee9be171c0ebf042e01ebbca5fbfd18d9
Artur Eshenbrener <strate@yandex.ru>
2016-04-16 17:27:51 +0300

    init

---
+++ README.markdown
@@ -0,0 +1,12 @@
+# sigh-spawn
+
+[![build status](https://circleci.com/gh/Strate/sigh-spawn.png)](https://circleci.com/gh/Strate/sigh-spawn)
+
+Sigh plugin for...
+
+## Example
+
+`npm install --save-dev sigh-spawn` then add something like this to your `sigh.js`:
+```javascript
+// TODO: example goes here
+```
---
+++ src/test/index.spec.js
@@ -0,0 +1,15 @@
+import _ from 'lodash'
+import Promise from 'bluebird'
+import { Bacon } from 'sigh-core'
+import Event from 'sigh/lib/Event'
+
+import spawn from '../'
+
+require('source-map-support').install()
+require('chai').should()
+
+describe('sigh-spawn', () => {
+  xit('TODO: should do something', () => {
+    // TODO:
+  })
+})
